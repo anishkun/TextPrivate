8d114f3d3be22d93bc79e3f931cc59fbe2f05271
anishkun <anish03anish@gmail.com>
2026-06-16 23:48:06 +0530

    feat: setup application class and manifest networking permissions

---
+++ app/src/main/java/com/anishkun/hidetext/HideTextApp.kt
@@ -0,0 +1,12 @@
+package com.anishkun.hidetext
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class HideTextApp : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        // Initialization code here
+    }
+}
