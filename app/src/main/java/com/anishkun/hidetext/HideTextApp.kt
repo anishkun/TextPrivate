ddcd5ec60290a775b04438bb031595f1944a5328
anishkun <anish03anish@gmail.com>
2026-06-21 16:51:57 +0530

    feat(panic): register AppLifecycleObserver with ProcessLifecycleOwner

--- app/src/main/java/com/anishkun/hidetext/HideTextApp.kt
+++ app/src/main/java/com/anishkun/hidetext/HideTextApp.kt
@@ -4,11 +4,19 @@ import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
 
 import timber.log.Timber
+import javax.inject.Inject
+import androidx.lifecycle.ProcessLifecycleOwner
 
 @HiltAndroidApp
 class HideTextApp : Application() {
+    
+    @Inject
+    lateinit var appLifecycleObserver: AppLifecycleObserver
+
     override fun onCreate() {
         super.onCreate()
         Timber.plant(Timber.DebugTree())
+        
+        ProcessLifecycleOwner.get().lifecycle.addObserver(appLifecycleObserver)
     }
 }
