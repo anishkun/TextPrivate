292631e080b1d055563e35a47afacc7bf831f978
anishkun <anish03anish@gmail.com>
2026-06-21 16:36:18 +0530

    feat(chat): define ChatIntent actions

---
+++ app/src/main/java/com/anishkun/hidetext/presentation/chat/ChatIntent.kt
@@ -0,0 +1,6 @@
+package com.anishkun.hidetext.presentation.chat
+
+sealed interface ChatIntent {
+    data class UpdateInput(val text: String) : ChatIntent
+    data object SendMessage : ChatIntent
+}
