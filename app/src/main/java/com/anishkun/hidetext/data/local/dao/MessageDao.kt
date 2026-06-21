c2a95b10340c1afc2993b52f5501b58c663b3a11
anishkun <anish03anish@gmail.com>
2026-06-21 20:01:39 +0530

    Update MessageDao to fetch messages by contactPhoneNumber

--- app/src/main/java/com/anishkun/hidetext/data/local/dao/MessageDao.kt
+++ app/src/main/java/com/anishkun/hidetext/data/local/dao/MessageDao.kt
@@ -9,8 +9,8 @@ import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface MessageDao {
-    @Query("SELECT * FROM messages ORDER BY timestamp ASC")
-    fun getAllMessages(): Flow<List<MessageEntity>>
+    @Query("SELECT * FROM messages WHERE contactPhoneNumber = :phoneNumber ORDER BY timestamp ASC")
+    fun getMessagesForContact(phoneNumber: String): Flow<List<MessageEntity>>
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertMessage(message: MessageEntity)
