d1560f267105ee0631d21a481ff7a382816f2e0a
anishkun <anish03anish@gmail.com>
2026-06-21 19:38:13 +0530

    Remove ToggleDisguise intent from ChatIntent

--- app/src/main/java/com/anishkun/hidetext/presentation/chat/ChatIntent.kt
+++ app/src/main/java/com/anishkun/hidetext/presentation/chat/ChatIntent.kt
@@ -3,5 +3,4 @@ package com.anishkun.hidetext.presentation.chat
 sealed interface ChatIntent {
     data class UpdateInput(val text: String) : ChatIntent
     data object SendMessage : ChatIntent
-    data object ToggleDisguise : ChatIntent
 }
