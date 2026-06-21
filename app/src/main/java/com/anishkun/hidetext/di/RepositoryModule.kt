0d9cb068859e63e7d8a725c24a5c054ec40edad7
anishkun <anish03anish@gmail.com>
2026-06-21 16:25:26 +0530

    feat(di): bind ChatRepositoryImpl in RepositoryModule

---
+++ app/src/main/java/com/anishkun/hidetext/di/RepositoryModule.kt
@@ -0,0 +1,20 @@
+package com.anishkun.hidetext.di
+
+import com.anishkun.hidetext.data.repository.ChatRepositoryImpl
+import com.anishkun.hidetext.domain.repository.ChatRepository
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class RepositoryModule {
+
+    @Binds
+    @Singleton
+    abstract fun bindChatRepository(
+        chatRepositoryImpl: ChatRepositoryImpl
+    ): ChatRepository
+}
